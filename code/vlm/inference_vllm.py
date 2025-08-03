22aa0fa873d98c42834e25d5784f8debfb4aa6ec
madehua <madehua@kuaishou.com>
2025-08-04 06:21:38 +0800

    1

--- code/vlm/inference_vllm.py
+++ code/vlm/inference_vllm.py
@@ -40,6 +40,7 @@ COT_PROMPT= """
 A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant \"\n    \"first thinks about the reasoning process in the mind and then provides the user with the answer. The reasoning \"\n    \"process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., \"\n    \"<think> reasoning process here </think><answer> answer here </answer>
 """
 
+
 def image_to_base64(image_path):
     """
     Convert image to base64 string
